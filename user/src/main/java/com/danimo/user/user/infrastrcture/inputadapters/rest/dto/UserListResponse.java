47569f5736754b3d8a367df79a2502da69433a79
9601dani <erickmorales201930699@cunoc.edu.gt>
2025-09-11 17:39:15 -0600

    .

--- user/src/main/java/com/danimo/user/user/infrastrcture/inputadapters/rest/dto/UserListResponse.java
+++ user/src/main/java/com/danimo/user/user/infrastrcture/inputadapters/rest/dto/UserListResponse.java
@@ -10,8 +10,9 @@ public class UserListResponse {
     private final String module;
     private final boolean manager;
     private final boolean enabled;
+    private final String locationId;
 
     public static UserListResponse fromDomain(User user) {
-        return new UserListResponse(user.getUsername(), user.getEmail(), user.getModule(), user.isManager(), user.isEnabled());
+        return new UserListResponse(user.getUsername(), user.getEmail(), user.getModule(), user.isManager(), user.isEnabled(), user.getLocationId().toString());
     }
 }
